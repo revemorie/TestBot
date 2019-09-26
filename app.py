12a2a4738d782cbc4241f33aec11717978b09f04
Adam Steinberg <Adam@Adam-MacAir.local>
2019-09-26 09:18:00 -0400

    random

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
-
+global array=["https://i.groupme.com/3024x4032.jpeg.d955cd324c2a473aa521d76be462e908","https://i.groupme.com/1124x1500.jpeg.338c4165e1da4698b5afddd28f8a90fb","https://i.groupme.com/563x1218.jpeg.7b7484ff24d64312ad8325f94650e544","https://i.groupme.com/1686x2250.jpeg.fcb140e1486d4bb88296cc0fe6ffc7e3"]
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
 
   # We don't want to reply to ourselves!
-  if "!cheeto" in data['text']:
+  if data['text'].lower() == "!cheeto":
     msg = 'Meow'
     send_message(msg)
 
@@ -24,16 +24,17 @@ def webhook():
 def send_message(msg):
   url  = 'https://api.groupme.com/v3/bots/post'
 
+  rand=random.choice(array)
   data ={
   'bot_id' : os.getenv('GROUPME_BOT_ID'),
           'text'   : msg,
   "attachments" : [
     {
       "type"  : "image",
-      "url"   : "https://i.groupme.com/3024x4032.jpeg.d955cd324c2a473aa521d76be462e908"
+      "url"   : rand
     }
   ],
-  'picture_url': "https://i.groupme.com/3024x4032.jpeg.d955cd324c2a473aa521d76be462e908"
+  'picture_url': rand
 }
   request = Request(url, urlencode(data).encode())
   json = urlopen(request).read().decode()
