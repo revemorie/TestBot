f1702f7dac2f88aa7793cda9e85c4119011a2bb8
Adam Steinberg <Adam@Adam-MacAir.local>
2019-09-26 12:43:45 -0400

    random

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 msg_choices=["Meow","Purr","Growl","lööps bröther"]
-special_array=["https://i.groupme.com/1280x720.jpeg.38d93ff6f1304ba2b94c0aaf602f990b","https://i.groupme.com/1125x1500.jpeg.09c008a31f954c1db86159d9c62c0a39","https://i.groupme.com/873x654.jpeg.95c9ee5907aa4822b8200b4349dbe332","https://i.groupme.com/1920x1080.jpeg.bad8a2da943847b08ea013b674e520d4","https://i.groupme.com/1224x1632.jpeg.67b08560e7ff4f07ae62932124137560"]
+special_array=["https://i.groupme.com/1500x2250.jpeg.fbc08bc4eff14029a23c7e684d602f1a","https://i.groupme.com/457x399.jpeg.5089079d5a4c4b5abae69ba1d77d413c","https://i.groupme.com/1266x2250.jpeg.76f6311bf2ef4d3e9fd75bf2d6fc8f0c","https://i.groupme.com/1280x720.jpeg.38d93ff6f1304ba2b94c0aaf602f990b","https://i.groupme.com/1125x1500.jpeg.09c008a31f954c1db86159d9c62c0a39","https://i.groupme.com/873x654.jpeg.95c9ee5907aa4822b8200b4349dbe332","https://i.groupme.com/1920x1080.jpeg.bad8a2da943847b08ea013b674e520d4","https://i.groupme.com/1224x1632.jpeg.67b08560e7ff4f07ae62932124137560"]
 array=["https://i.groupme.com/4032x3024.jpeg.684ffe5536a44b69bfc88c4567e10515","https://i.groupme.com/1124x1500.jpeg.94eeffa8faec4036bd670e1a8837e42f","https://i.groupme.com/1798x1126.jpeg.5c5f21ace93e4e76add41a30f6457680","https://i.groupme.com/3024x4032.jpeg.fa24f79a44d74a6796b20a90432edba8","https://i.groupme.com/1688x2250.jpeg.cb2bf654cd614b19853bc01edb4e96b1","https://i.groupme.com/3024x4032.jpeg.d955cd324c2a473aa521d76be462e908","https://i.groupme.com/1124x1500.jpeg.338c4165e1da4698b5afddd28f8a90fb","https://i.groupme.com/563x1218.jpeg.7b7484ff24d64312ad8325f94650e544","https://i.groupme.com/1686x2250.jpeg.fcb140e1486d4bb88296cc0fe6ffc7e3"]
 @app.route('/', methods=['POST'])
 def webhook():
