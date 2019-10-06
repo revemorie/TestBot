e31cf4b8bb151ba79d2582ed49435eb6933df0ff
Adam Steinberg <Adam@Adam-MacAir.local>
2019-10-06 12:07:27 -0400

    random

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def webhook():
     msg=""
     for i in range(10):
       if int(toPrint[i][0]) != 0:
-        msg+=str(toPrint[i][1])+" received "+str(toPrint[i][0])+" likes per post for the past 10000 messages\n"
+        msg+=str(toPrint[i][1])+" received "+str(round(toPrint[i][0],2))+" likes per post for the past 10000 messages\n"
 
     print(msg)
     send_msg(msg)
