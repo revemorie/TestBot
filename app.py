1ba7eb9b45147cd6a4db0c4c5ae017250ae36ddd
Adam Steinberg <Adam@Adam-MacAir.local>
2019-10-06 23:12:53 -0400

    random

--- app.py
+++ app.py
@@ -184,8 +184,8 @@ def webhook():
     leaderboardName.clear()
     leaderboardId.clear()
     leaderboardMsgNum.clear()
- '''   
-  elif ("i’m" in data['text'].lower() or "i'm" in data['text'].lower() ) and data['sender_type']!='bot' and ((time.time())-countTime)>3600:
+  
+  elif ("i’m" in data['text'].lower() or "i'm" in data['text'].lower() ) and data['sender_type']!='bot' and ((time.time())-countTime)>3600 and False:
     countTime=time.time()
     amNum=data['text'].lower().find("i'm")
     if amNum==-1:
@@ -194,7 +194,6 @@ def webhook():
     am=data['text'][amNum+4:]
     msg="Hi "+am+", I'm CheetoBot"
     send_msg(msg)
-'''
 
   return "ok", 200
 
