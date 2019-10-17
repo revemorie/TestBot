c7d19a3d4c0ee963b5ab7ce474f3355ae753c06f
Adam Steinberg <Adam@Adam-MacAir.local>
2019-10-17 10:11:48 -0400

    Update #2

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
+#imports
 import os
 import sys
 import json
 import random
 import requests
 import time
-
 from flask import Flask, request
 
+#define our flask app
 app = Flask(__name__)
 
+#Method will automatically execute when our endpoint receives a POST call
 @app.route('/', methods=['POST'])
 def msg_received_from_group():
 
+  #Format the data we receive as a JSON
   data = request.get_json()
   log('{}'.format(data))
   
+  #Check the text of the message sent to the chat to see if it matches our command word
   if data['text'].lower() == "!test":
-   send_msg("Hello World!")
+	send_msg("Hello World!")
 
   return "ok", 200
 
-  
+ 
+#Sends a message to the chat that the bot originates from
 def send_msg(msg):
 
   url  = 'https://api.groupme.com/v3/bots/post'
@@ -32,7 +37,28 @@ def send_msg(msg):
         
   request = requests.post(url=url, data=data)
 
+#sends a picture and a message to the chat
+#Picture URL must be registered with GroupMe first
+def send_message_picture(msg, picURL):
 
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data ={
+  'bot_id' : os.getenv('GROUPME_BOT_ID'),
+  'text'   : msg,
+  "attachments" : [
+    {
+      "type"  : "image",
+      "url"   : picURL
+    }
+  ],
+  'picture_url': rand
+}
+
+request = requests.post(url=url, data=data)
+
+
+#logging function to help debug
 def log(msg):
   print(str(msg))
   sys.stdout.flush()
