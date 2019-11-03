5127c6d64216fb15a778b2d9adbeacda6fc9f252
revemorie <54751784+revemorie@users.noreply.github.com>
2019-11-02 20:30:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def msg_received_from_group():
   log('{}'.format(data))
   
   #Check the text of the message sent to the chat to see if it matches our command word
-  if data['text'].lower() == "!test":
-    send_msg("Hello World!")
+  if data['text'].lower() == "who is joe" or "who's joe":
+    send_msg("Joe mama gottem")
 	
 
   elif data['text'].lower() == "!testpic":
